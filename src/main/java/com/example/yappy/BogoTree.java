ae5c23c08aea4ca129df28f3dd97f2c5385809de
jamesread <contact@jwread.com>
2016-03-06 10:41:14 +0000

    .

--- src/main/java/com/example/yappy/BogoTree.java
+++ src/main/java/com/example/yappy/BogoTree.java
@@ -27,15 +27,16 @@ public class BogoTree<T> implements Iterable<T> {
 
 			@Override
 			public boolean hasNext() {
-				// TODO Auto-generated method stub
 				return false;
 			}
 
 			@Override
 			public T next() {
-				// TODO Auto-generated method stub
 				return null;
 			}
+
+			@Override
+			public void remove() {}
 			
 		};
 	}
